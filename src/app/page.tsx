9fcbd77f41b6b4f2c6ddde5244acd7f32d744c2a
Abdeldjelil BABAHAMED <8852906+CytrusGallium@users.noreply.github.com>
2024-12-22 13:26:55 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center mt-4 h-screen">
+    <div className="flex flex-col items-center justify-center mt-4" style={{ height: "80vh" }}>
       <div className="fond-bold text-4xl">
         Kompleta
       </div>
